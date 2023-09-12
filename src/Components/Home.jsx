88e1cb024f0826b406a30375ef9e8aa1b126d231
Unnati Agarwal <108344633+unnati14ag@users.noreply.github.com>
2023-09-13 03:30:00 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/socials/Cloud enthusiast.jpg";
+import image from "../images/Cloud enthusiast.jpg";
 
 const imageAltText = "A macbook with notebooks. This is a background photo";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/socials/coder's desk.jpg";
+import image from "../images/coder's desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
